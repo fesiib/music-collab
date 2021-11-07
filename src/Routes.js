1e9b02bbf32df97020a726ac93c2e0674eead5a9
BaurTokenov <baur.tokenov@kaist.ac.kr>
2021-11-07 12:06:59 +0900

    starter project page

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-} from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import App from "./App";
-import ProjectPage from './pages/ProjectPage';
+import App from './App'
+import ProjectPage from './pages/ProjectPage'
 
 class Routes extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={App}/>
-                    <Route exact path="/project/:projectId" component={ProjectPage} />
-                </Switch>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route
+            exact
+            path="/project/:projectId"
+            component={() => (
+              <ProjectPage
+                {...{
+                  name: 'A Tender Heart',
+                  description:
+                    'Light and dreamy, with kids/ children gentle/ lullaby elements featuring heartfelt piano',
+                  author: 'Heidi',
+                  collaborators: 7,
+                  versions: 13
+                }}
+              />
+            )}
+          />
+        </Switch>
+      </Router>
+    )
+  }
 }
 
-export default Routes;
+export default Routes
