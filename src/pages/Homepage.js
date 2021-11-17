1cccc6435dd4ebfc7a775d0d06006ec7a370cfc9
fesiib <tlekbay.b@gmail.com>
2021-11-17 13:10:13 +0900

    Homepage: MusicList connected to reducers & small changes to the database

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,7 +5,7 @@ import SlidingPanel from 'react-sliding-side-panel';
 import 'react-sliding-side-panel/lib/index.css';
 
 import GenericButton from '../components/GenericButton';
-import MusicList from '../components/MusicList';
+import MusicList, { TRANSFORM_AUTHOR, TRANSFORM_OWNER } from '../components/MusicList';
 import SearchBar from '../components/SearchBar';
 import withHeader from '../hocs/withHeader';
 import { useDispatch, useSelector } from 'react-redux';
@@ -69,9 +69,12 @@ function Homepage(props) {
                     <h1 className="p-10 text-center">
                         My Projects
                     </h1>
-                    <MusicList headers={
-                        ['trackTitle', 'genre', 'cntVersions', 'cntCollab', 'duration']
-                    } votes={true} className={"h-96"}/>
+                    <MusicList 
+                        headers={['trackTitle', 'genre', 'cntVersions', 'cntCollab', 'duration']}
+                        votes={true}
+                        className={"h-96"}
+                        transform={TRANSFORM_OWNER}
+                    />
                     <div className="flex justify-center m-5">    
                         <GenericButton title={"Create New Project"} className="text-xl w-2/5 p-2" />
                     </div>
@@ -99,11 +102,10 @@ function Homepage(props) {
                         Contributions
                     </h1>
                     <MusicList 
-                        headers={
-                        ['trackTitle', 'genre', 'cntVersions', 'cntCollab', 'duration']
-                        }
-                        filter={{}}
-                        votes={true} className={"h-96"}
+                        headers={['trackTitle', 'genre', 'cntVersions', 'cntCollab', 'duration']}
+                        votes={true}
+                        className={"h-96"}
+                        transform={TRANSFORM_AUTHOR}
                     />
                     <h2 className="p-3 text-center">
                         My Comments
