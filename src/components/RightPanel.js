5bba260a168f4dd43b52ff4cdb7135c990c43c93
BaurTokenov <baur.tokenov@kaist.ac.kr>
2021-11-18 18:59:29 +0900

    handling null values

--- src/components/RightPanel.js
+++ src/components/RightPanel.js
@@ -1,16 +1,20 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import ReactTimeAgo from 'react-time-ago';
+import ReactTimeAgo from "react-time-ago";
 
-import TimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en.json';
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en.json";
 
 import { closePanel } from "../reducers/homepage/homepagePanel";
 
-import SlidingPanel from 'react-sliding-side-panel';
+import SlidingPanel from "react-sliding-side-panel";
 import GenericButton from "./GenericButton";
-import MusicList, { TRANSFORM_POPULAR_SINGLE, TRANSFORM_RECENT, TRANSFORM_RECENT_SINGLE } from "./MusicList";
+import MusicList, {
+    TRANSFORM_POPULAR_SINGLE,
+    TRANSFORM_RECENT,
+    TRANSFORM_RECENT_SINGLE
+} from "./MusicList";
 import CustomTag from "./CustomTag";
 import TagList from "./TagList";
 
@@ -18,17 +22,22 @@ import TagList from "./TagList";
 
 function getCollaborators(project) {
     let collaborators = new Set();
-    for (let versionId in project.versions) {
-        collaborators.add(project.versions[versionId].metaInfo.authorId);
+    if (!project) {
+        return collaborators;
+    }
+    if (project) {
+        for (let versionId in project.versions) {
+            collaborators.add(project.versions[versionId].metaInfo.authorId);
+        }
     }
     return collaborators;
 }
 
 function SingleCollaborator(props) {
     return (
-        <div className = "flex flex-col m-4">
+        <div className="flex flex-col m-4">
             <div className="rounded-full h-24 w-24 overflow-hidden">
-                <img src= {props.profileImage} className="object-cover h-24" />
+                <img src={props.profileImage} className="object-cover h-24" />
             </div>
             <p className="text-center"> {props.name} </p>
         </div>
@@ -36,7 +45,7 @@ function SingleCollaborator(props) {
 }
 
 function AllCollaborators(props) {
-    const { profiles } = useSelector(state => state.database);
+    const { profiles } = useSelector((state) => state.database);
 
     let collaborators = props.collaborators;
     if (collaborators.length > 10) {
@@ -44,18 +53,19 @@ function AllCollaborators(props) {
     }
     return (
         <div className="flex flex-wrap justify-evenly">
-            {
-                collaborators.map((authorId, idx) => {
-                    const profileImage = profiles[authorId].metaInfo.profileImage;
-                    const name = profiles[authorId].metaInfo.name;
-                    const customKey = authorId;
-                    return (
-                        <div key={customKey}>
-                            <SingleCollaborator name={name} profileImage={profileImage}/>
-                        </div>
-                    );
-                })
-            }
+            {collaborators.map((authorId, idx) => {
+                const profileImage = profiles[authorId].metaInfo.profileImage;
+                const name = profiles[authorId].metaInfo.name;
+                const customKey = authorId;
+                return (
+                    <div key={customKey}>
+                        <SingleCollaborator
+                            name={name}
+                            profileImage={profileImage}
+                        />
+                    </div>
+                );
+            })}
         </div>
     );
 }
@@ -63,13 +73,15 @@ function AllCollaborators(props) {
 function RightPanel(props) {
     const dispatch = useDispatch();
 
-    const { projects, profiles } = useSelector(state => state.database);
-    const { panelState, sortType, projectId, versionId } = useSelector(state => state.homepagePanel);
-    const { tabIndex } = useSelector(state => state.tabInfo);
+    const { projects, profiles } = useSelector((state) => state.database);
+    const { panelState, sortType, projectId, versionId } = useSelector(
+        (state) => state.homepagePanel
+    );
+    const { tabIndex } = useSelector((state) => state.tabInfo);
 
     const _closePanel = () => {
         dispatch(closePanel());
-    }
+    };
 
     let transform = TRANSFORM_POPULAR_SINGLE;
     if (tabIndex === 0 || sortType === TRANSFORM_RECENT) {
@@ -85,16 +97,15 @@ function RightPanel(props) {
 
     const collaborators = Array.from(getCollaborators(project));
 
-    const trackTitle = project.metaInfo.trackTitle;
-    const tags = project.metaInfo.tags;
-    const lastModified = project.metaInfo.lastModified;
-    const creationTime = project.metaInfo.creationTime;
-    const ownerName = profiles[project.metaInfo.ownerId].metaInfo.name;
+    const trackTitle = project?.metaInfo?.trackTitle;
+    const tags = project?.metaInfo?.tags;
+    const lastModified = project?.metaInfo?.lastModified;
+    const creationTime = project?.metaInfo?.creationTime;
+    const ownerName = profiles[project?.metaInfo?.ownerId]?.metaInfo?.name;
 
     const redirectToProjectPage = () => {
         console.log("redirect");
-    }
-
+    };
 
     useEffect(() => {
         TimeAgo.addDefaultLocale(en);
@@ -102,21 +113,31 @@ function RightPanel(props) {
 
     return (
         <SlidingPanel
-            type={'right'}
+            type={"right"}
             isOpen={panelState}
             size={30}
             backdropClicked={_closePanel}
-            panelClassName=' bg-gray-50'
+            panelClassName=" bg-gray-50"
         >
-            <div className='bg-white'>
+            <div className="bg-white">
                 <div className="flex flex-row justify-between">
                     <h3 className="text-left p-4"> About Project </h3>
-                    <GenericButton title={"Go to Project"} className="m-3" onClick={redirectToProjectPage}/>
+                    <GenericButton
+                        title={"Go to Project"}
+                        className="m-3"
+                        onClick={redirectToProjectPage}
+                    />
                 </div>
 
                 <div className="pl-5 pb-5 text-left">
                     <p> Title: </p>
-                    <p className="text-4xl underline hover:text-gray-600 cursor-pointer" onClick={redirectToProjectPage}> {trackTitle} </p>
+                    <p
+                        className="text-4xl underline hover:text-gray-600 cursor-pointer"
+                        onClick={redirectToProjectPage}
+                    >
+                        {" "}
+                        {trackTitle}{" "}
+                    </p>
                     <p className="pt-5"> Owner: </p>
                     <p className="text-2xl text-gray-600"> {ownerName} </p>
                 </div>
@@ -135,24 +156,32 @@ function RightPanel(props) {
                         <GenericButton title={"Go To Project"} />
                     </div>      
                 </div> */}
-                <div className='pl-5'>
-                    <TagList tags={tags}/>
+                <div className="pl-5">
+                    <TagList tags={tags} />
                 </div>
                 <div className="p-5 flex flex-row justify-between">
-                    <div className="text-xs text-gray-500"> 
-                        Last Modified: 
-                        <br/>
-                        <ReactTimeAgo date={lastModified} timeStyle="twitter-minute" locale="en-US"/>
+                    <div className="text-xs text-gray-500">
+                        Last Modified:
+                        <br />
+                        <ReactTimeAgo
+                            date={lastModified}
+                            timeStyle="twitter-minute"
+                            locale="en-US"
+                        />
                     </div>
-                    <div className="text-xs text-gray-500"> 
-                        Created: 
-                        <br/>
-                        <ReactTimeAgo date={creationTime} timeStyle="twitter-minute" locale="en-US"/>
+                    <div className="text-xs text-gray-500">
+                        Created:
+                        <br />
+                        <ReactTimeAgo
+                            date={creationTime}
+                            timeStyle="twitter-minute"
+                            locale="en-US"
+                        />
                     </div>
                 </div>
                 <h2 className="m-5 text-left"> Versions </h2>
-                <MusicList 
-                    headers={['playButton', 'author', 'duration']}
+                <MusicList
+                    headers={["playButton", "author", "duration"]}
                     votes={true}
                     className={"max-h-96"}
                     transform={transform}
@@ -161,10 +190,8 @@ function RightPanel(props) {
                     panel={false}
                 />
                 <div>
-                <h2 className="pl-5 pt-5 text-left">
-                        Collaborators
-                    </h2>
-                    <AllCollaborators collaborators={collaborators}/>
+                    <h2 className="pl-5 pt-5 text-left">Collaborators</h2>
+                    <AllCollaborators collaborators={collaborators} />
                 </div>
             </div>
         </SlidingPanel>
