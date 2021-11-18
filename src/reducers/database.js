378abe1e547dcbaa1d05640c2fd9064eedd7898a
fesiib <tlekbay.b@gmail.com>
2021-11-18 22:45:10 +0900

    database: change dummy profile picture

--- src/reducers/database.js
+++ src/reducers/database.js
@@ -41,7 +41,7 @@ const DUMMY_PROFILE_1 = {
     metaInfo: {
         name: "Helena",
         communityRating: 0,
-        profileImage: "https://images.askmen.com/1080x540/2016/01/25-021526-facebook_profile_picture_affects_chances_of_getting_hired.jpg",
+        profileImage: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F00%2Fec%2F6b%2F00ec6b1a19a8dd9dee3949d4f7b09c1b.jpg&f=1&nofb=1",
     },
     projectIds: ['sunnyDay'],
     versionIds: [
