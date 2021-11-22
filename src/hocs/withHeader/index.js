ce199f26bf6cb9863571aed10708d7fd83ee3ec5
fesiib <tlekbay.b@gmail.com>
2021-11-22 23:35:22 +0900

    Homepage: Table header overlapping header

--- src/hocs/withHeader/index.js
+++ src/hocs/withHeader/index.js
@@ -5,7 +5,7 @@ const withHeader = (Page) => {
   return (props) => {
     return (
       <div className="w-full flex flex-col">
-        <div className="w-full fixed">
+        <div className="w-full fixed z-20">
           <Header />
         </div>
         <div className="mt-32">
