596a18f65147d8ddf2cb76a286df58b8224673b1
fesiib <tlekbay.b@gmail.com>
2021-11-23 00:29:29 +0900

    prototype: reorganize comments

--- src/App.js
+++ src/App.js
@@ -14,18 +14,19 @@ function App() {
 
   return (
     <div className="flex flex-col justify-center align-middle ml-3 p-5">
-      <div className=""> Hello its Prototype page, you can reset the state of the website here! </div>
-      <div> Also you can check if you can upload something here! </div>
       <Link to="/">
-        <button className="bg-blue-300 text-black mt-4 w-40">
+        <button className="bg-blue-300 text-black my-4 w-40">
           Go to homepage
         </button>
       </Link>
 
-      <button onClick={() => dispatch(resetApp())} className="bg-black my-4 w-40">
+      <div className=""> Hello its Prototype page, you can reset the state of the website here! </div>
+      <button onClick={() => dispatch(resetApp())} className="bg-black w-40">
         RESET
       </button>
 
+      <div className="mt-4"> Also you can check if you can upload something here! </div>
+      
       <UploadAudio/>
     </div>
   )
